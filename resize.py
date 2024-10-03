5f5e4eaf46ce31cd312d3dbaa55a358fdd2ef58e
gungui98 <phi.nguyen.uet@gmail.com>
2024-10-03 10:44:47 +0700

    update

--- resize.py
+++ resize.py
@@ -6,7 +6,7 @@ def resize_images(folder_path, target_width):
     print(folder_path)
     for filename in os.listdir(folder_path):
         print(filename)
-        if filename.endswith(('.JPG', '.jpg', '.jpeg', '.png', '.gif', '.bmp')) and "MCD" in filename:
+        if filename.endswith(('.JPG', '.jpg', '.jpeg', '.png', '.gif', '.bmp')):
             img_path = os.path.join(folder_path, filename)
             img = cv2.imread(img_path)
             if img is None:
@@ -16,12 +16,12 @@ def resize_images(folder_path, target_width):
             aspect_ratio = width / height
             new_height = int(target_width / aspect_ratio)
             resized_img = cv2.resize(img, (target_width, new_height))
-            cv2.imwrite(img_path, resized_img, [cv2.IMWRITE_JPEG_QUALITY, 60])
+            cv2.imwrite(img_path, resized_img, [cv2.IMWRITE_JPEG_QUALITY, 96])
             print(f"Resized image: {img_path}")
 
 # Example usage
-folder_path = './assets/images'
-target_width = 1280
+folder_path = './assets/gallery'
+target_width = 1920
 resize_images(folder_path, target_width)
 
 
